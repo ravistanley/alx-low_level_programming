21b5353bda7997f7f3732c5fe51d0ca8a98081d5
Stanley <ravistanley99@gmail.com>
2022-09-09 05:27:06 -0700

    commit to 101-qoute.c

--- 0x00-hello_world/101-qoute.c
+++ 0x00-hello_world/101-qoute.c
@@ -1,19 +1,16 @@
-#include<unistd.h>
+#include <unistd.h>
 
 /**
- * main - prints the standard error sandwiched around the standard outpu/t
- *
+ * main - prints the standard error
  *
  * Return: returns 1
  */
 
 int main(void)
-
 {
-
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-
 
 	return (1);
 }
+
+
