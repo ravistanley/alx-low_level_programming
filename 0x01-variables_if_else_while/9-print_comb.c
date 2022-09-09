1c9d64d6697caaa5a6b2c9f66c2fd5682cbd1d4a
Stanley <ravistanley99@gmail.com>
2022-09-09 10:47:21 -0700

    prints possible combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+		if (num == 57)
+		{
+			break;
+		}
+	}
+
+	putchar(',');
+	putchar(' ');
+
+	putchar('\n');
+
+	return (0);
+}
