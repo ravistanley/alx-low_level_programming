5728cd1c8764831affa2ddaca0704d2a5164e79b
Stanley <ravistanley99@gmail.com>
2022-09-09 09:48:29 -0700

    prints single base 10 digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0 (Success)
+ */
+
+int main(void)
+{
+	int numberz;
+
+	for (numberz = 0; numberz < 10; numberz++)
+	{
+		putchar(numberz);
+	}
+
+	putchar('\n');
+	return (0);
+}
