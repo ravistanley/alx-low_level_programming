51d62b0135f7f0f6e9264fd613c11bf889a24310
Stanley <ravistanley99@gmail.com>
2022-09-14 13:37:55 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - increases and decreases until 98
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, result;
+
+	i = 0;
+	result = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			result += 1;
+		i++;
+	}
+
+	printf("%d\n", result);
+	return (0);
+}
