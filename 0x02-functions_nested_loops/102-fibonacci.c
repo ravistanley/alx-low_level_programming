1613059e8a8cfe0b3622a439b4b74e028dc54131
Stanley <ravistanley99@gmail.com>
2022-09-14 13:48:32 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
+
+int main(void)
+{
+	long int n;
+	long int num_1 = 0;
+	long int num_2 = 1;
+	long int nextTerm;
+
+	for (n = 0; n < 50; ++n)
+	{
+		nextTerm = num_1 + num_2;
+		num_1 = num_2;
+		num_2 = nextTerm;
+
+		if (n != 49)
+		{
+			printf("%ld, ", nextTerm);
+		}
+
+		else
+		{
+			printf("%ld\n", nexTerm);
+		}
+	}
+	return (0);
+}
